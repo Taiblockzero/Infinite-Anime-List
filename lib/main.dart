b628fabe9ca9e34313c3c08d9109019a2288aed9
Taiblockzero <taiblockzero@gmail.com>
2021-08-18 10:42:28 +0300

    tried to use the api to commit 10 new numbers but had problems with the json decoding

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'dart:math';
+
+import 'package:http/http.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,6 +33,19 @@ class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  var url;
+  var _animeInfoJson = [];
+
+  void fetchPosts(var urlParam) async {
+    try {
+      final response = await get(Uri.parse(urlParam));
+      final jsonData = jsonDecode(response.body);
+      setState(() {
+        _animeInfoJson = (jsonData[data][attributes][titles][en] as List);
+      });
+    } catch (err) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +56,7 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(var animeName) {
     return ListTile(
       title: Text(
         pair.asPascalCase,
@@ -55,9 +73,29 @@ class _RandomWordsState extends State<RandomWords> {
 
           final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            //generate 10 random numbers and put them in a temporary list
+            var randomList = [];
+            Random random = new Random();
+
+            for (var j = 0; j < 10; j++) {
+              int randomNum = random.nextInt(50000);
+              randomList.add(randomNum);
+            }
+            //get the animes with those IDs from the mal api
+            var animeNames = [];
+            for (var randNum in randomList) {
+              url = "https://kitsu.io/api/edge/anime/" + randNum.toString();
+              fetchPosts(url);
+              //put the anime name into the list
+
+              _suggestions.add(_animeInfoJson[data][attributes][titles][en]);
+            }
+
+            //put the 10 anime names into the list
+            _suggestions.addAll(generateWordPairs().take(10));
           }
-          return _buildRow(_suggestions[index]);
+
+          return _buildRow(randomNum);
         });
   }
 }
